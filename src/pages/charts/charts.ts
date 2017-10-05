0d2f8a79a44f78d2fbd746bc3455f6ec6589bfd7
leomer <leomer@redapesolutions.com>
2017-10-05 17:52:21 +0800

    Added arrows for chart mascot

--- src/pages/charts/charts.ts
+++ src/pages/charts/charts.ts
@@ -20,6 +20,7 @@ export class ChartsPage {
   priceDescription: Array<string> = ['','51', '52', '53']
   holdingDescription: Array<string> = ['','100', '200', '300']
   valueDescription: Array<string> = ['','5100', '10400', '15900']
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.loadScreen(this.tapCount);
   }
@@ -28,11 +29,19 @@ export class ChartsPage {
     console.log('ionViewDidLoad ChartsPage');
   }
 
-  changeMascot() {
-    if(this.tapCount < 3) {
-      this.tapCount += 1;
+  changeMascot(direction: string) {
+    if(direction == 'left') {
+      if(this.tapCount == 1) {
+        this.tapCount = 3;
+      } else {
+        this.tapCount--;
+      }
     } else {
-      this.tapCount = 1;
+      if(this.tapCount < 3) {
+        this.tapCount += 1;
+      } else {
+        this.tapCount = 1;
+      }
     }
     this.loadScreen(this.tapCount)
   }
